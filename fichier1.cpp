21ce97423fd5ade4d198bd3992a274427dd2c5e0
Tintin33 <martinc62.mc@gmail.com>
2022-04-04 11:08:02 +0200

    Update fichier1.cpp
    commit Martin

--- fichier1.cpp
+++ fichier1.cpp
@@ -54,8 +54,106 @@ class Livres : public Media{
     int nb_pages;
     string collection;
     string resume;
-
 };
+
+class CD : public Media {
+    protected :
+    int duree;
+    int nb_piste;
+    string maison_de_prod;
+};
+
+class VHS : public Media {
+  protected :
+    int duree;
+    string maison_de_prod;
+};
+
+class Ressources_Num : public Media {
+  protected :
+      string type;
+      int taille;
+      string URL;
+};
+
+class Revues : public Livres {
+  protected :
+    string editeur;
+    int nb_articles;
+};
+
+class DVD : public VHS {
+  protected :
+  int chapitre;
+};
+
+// Class Media
+
+cout << "Entrez l'auteur : " << endl;
+getline( cin, auteur);
+
+cout << "Entrez le titre : " << endl;
+getline( cin, titre);
+
+cout << "Entrez la date de publication : " << endl;
+getline( cin, date_de_publi);
+
+cout << "Entrez l'ID : " << endl;
+getline( cin, ID);
+
+// Class Livres
+
+cout << "Entrez le nombre de pages : " << endl;
+getline( cin, nb_pages);
+
+cout << "Entrez la collection : " << endl;
+getline( cin, collection);
+
+cout << "Entrez le résumé : " << endl;
+getline( cin, resume);
+
+// Class Revues
+
+cout << "Entrez l'éditeur : " << endl;
+getline( cin, editeur);
+
+cout << "Entrez le nombre d'articles :  : " << endl;
+getline( cin, nb_articles);
+
+// Class CD
+
+cout << "Entrez la durée du CD : " << endl;
+getline( cin, duree);
+
+cout << "Entrez le nombre de pistes : " << endl;
+getline( cin, nb_piste);
+
+cout << "Entrez la maison de production : " << endl;
+getline( cin, maison_de_prod);
+
+// Class VHS
+
+cout << "Entrez la durée : " << endl;
+getline( cin, duree);
+
+cout << "Entrez la maison de production : " << endl;
+getline( cin, maison_de_prod);
+
+// CLass DVD
+
+cout << "Entrez le nombres de chapitres : " << endl;
+getline( cin, chapitre);
+
+// Class Ressources_Num
+
+cout << "Entrez le Type : " << endl;
+getline( cin, type);
+
+cout << "Entrez la taille : " << endl;
+getline( cin, taille);
+
+cout << "Entrez le chemin d'accès : " << endl;
+getline( cin, URL);
 
 
 int main(int argc, char const *argv[])
